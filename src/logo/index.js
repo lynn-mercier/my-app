fda0ebc04f0c20f3fd181177cbd92dd123ba4793
Lynn Mercier <44537076+lynn-mercier@users.noreply.github.com>
2019-09-22 16:33:13 -0700

    refactor: extract Logo component (#3)

---
+++ src/logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './index.scss';
+
+function Logo() {
+  return (
+    <img src={logo} className="App-logo" alt="logo" />
+  );
+}
+
+export default Logo;
