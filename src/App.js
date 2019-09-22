c2b9f9f59a6158e0e7f4a8686ece38a04c93b7ee
lynnjepsen <lynn.ej.mercier@gmail.com>
2019-09-22 16:31:31 -0700

    refactor: extract Logo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from './logo';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
